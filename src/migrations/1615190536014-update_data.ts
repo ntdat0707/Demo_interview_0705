5ef6a01184f00dbc23747dd73bc3870c4d3f0ccf
dat <dat02.nguyen@unibiz.io>
2021-03-08 15:12:40 +0700

    tested new table

--- src/migrations/1615190536014-update_data.ts
+++ src/migrations/1615190536014-update_data.ts
@@ -5,13 +5,11 @@ export class UpdateData1615190536014 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "solution_image" DROP COLUMN "is_avatar"`);
-    await queryRunner.query(`ALTER TABLE "banner" ADD "index" integer DEFAULT '0'`);
     await queryRunner.query(`ALTER TABLE "branch" ALTER COLUMN "description" DROP NOT NULL`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "branch" ALTER COLUMN "description" SET NOT NULL`);
-    await queryRunner.query(`ALTER TABLE "banner" DROP COLUMN "index"`);
     await queryRunner.query(`ALTER TABLE "solution_image" ADD "is_avatar" boolean NOT NULL DEFAULT false`);
   }
 }
